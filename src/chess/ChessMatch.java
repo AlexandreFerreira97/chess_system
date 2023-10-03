5ab6036eb5eb4cf60dea19d2fae068b42b11f327
Alexandre <alexandrefaria3012@gmail.com>
2023-10-03 00:08:24 -0300

    update

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -25,7 +25,7 @@ public class ChessMatch {
 		return mat;
 	}
 	
-	public ChessPiece peformChessMove(ChessPosition sourcePosition, ChessPosition targetPosition) {
+	public ChessPiece performChessMove(ChessPosition sourcePosition, ChessPosition targetPosition) {
 		Position source = sourcePosition.toPosition();
 		Position target = targetPosition.toPosition();
 		validateSourcePosition(source);
@@ -33,7 +33,18 @@ public class ChessMatch {
 		return (ChessPiece)capturedPiece;
 	}
 	
-	private void validateSourcePosition(Position)
+	private Piece makeMove(Position source, Position target) {
+		Piece p = board.removePiece(source);
+		Piece capturedPiece = board.removePiece(target);
+		board.placePiece(p, target);
+		return capturedPiece;
+	}
+	
+	private void validateSourcePosition(Position position) {
+		if(!board.thereIsAPiece(position)) {
+			throw new ChessException("There is no piece on source position");
+		}
+	}
 	
 	private void placeNewPiece(char column, int row, ChessPiece piece) {
 		board.placePiece(piece, new ChessPosition(column, row).toPosition());
